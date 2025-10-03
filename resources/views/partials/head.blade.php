adf58f64acfd17849afe21cb6089c953afdcffd7
Thiago Garcia <thiago.garcia@tagee.tech>
2025-10-03 02:03:27 -0300

    Clean: Remove referências a favicons não utilizados

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,9 +3,7 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/favicon-new.ico" sizes="any">
-<link rel="icon" href="/favicon-new.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon-new.png">
+<link rel="icon" href="/favicon-new.ico" type="image/x-icon">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
