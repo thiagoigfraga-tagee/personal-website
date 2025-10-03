c20c91defee78be4e3608fa8b7540bc6ee90bf4c
Thiago Garcia <thiago.garcia@tagee.tech>
2025-10-03 01:56:10 -0300

    Fix: Renomeia favicons para forçar atualização do cache

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -3,9 +3,9 @@
 
 <title>{{ $title ?? config('app.name') }}</title>
 
-<link rel="icon" href="/favicon.ico?v=2" sizes="any">
-<link rel="icon" href="/favicon.svg?v=2" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png?v=2">
+<link rel="icon" href="/favicon-new.ico" sizes="any">
+<link rel="icon" href="/favicon-new.svg" type="image/svg+xml">
+<link rel="apple-touch-icon" href="/apple-touch-icon-new.png">
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
